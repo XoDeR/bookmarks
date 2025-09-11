6adc66c4e04c3802f1d2c88570f4d8c323421ecf
XoDeR <xoder.vs@gmail.com>
2025-09-11 16:19:32 +0200

    migrations for links and groups

--- database/migrations/2025_09_11_132308_create_links_table.php
+++ database/migrations/2025_09_11_132308_create_links_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->string('link', 2048);
+            $table->unsignedBigInteger('user_id')->default(1);
+
+            $table->index('user_id', 'links_user_id_foreign');
+
+            $table->foreign('user_id', 'links_user_id_foreign')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
--- database/migrations/2025_09_11_132324_create_groups_table.php
+++ database/migrations/2025_09_11_132324_create_groups_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,32 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->unsignedBigInteger('parent_group_id')->nullable();
+            $table->json('query_options')->nullable();
+            $table->integer('links_count')->default(0);
+            $table->unsignedBigInteger('user_id');
+
+            $table->index('user_id', 'groups_user_id_foreign');
+            $table->index('parent_group_id', 'groups_parent_group_id_foreign');
+
+            $table->foreign('parent_group_id', 'groups_parent_group_id_foreign')
+                  ->references('id')->on('groups')
+                  ->onDelete('set null');
+
+            $table->foreign('user_id', 'groups_user_id_foreign')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
             $table->timestamps();
+        });
+
+        Schema::create('groupables', function (Blueprint $table) {
+            $table->foreignId('group_id')->constrained()->cascadeOnDelete();
+
+            $table->morphs('groupable');
+
+            $table->unique(['group_id', 'groupable_id', 'groupable_type']);
         });
     }
 
@@ -22,6 +46,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('taggables');
         Schema::dropIfExists('groups');
     }
 };
