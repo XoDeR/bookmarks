d914ba745bfca2d496da48b11c38c47263f726df
XoDeR <xoder.vs@gmail.com>
2025-09-11 16:22:27 +0200

    created PublicLink model (--all) and migration for it

---
+++ app/Http/Controllers/PublicLinkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PublicLink;
+use App\Http\Requests\StorePublicLinkRequest;
+use App\Http\Requests\UpdatePublicLinkRequest;
+
+class PublicLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePublicLinkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PublicLink $publicLink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PublicLink $publicLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePublicLinkRequest $request, PublicLink $publicLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PublicLink $publicLink)
+    {
+        //
+    }
+}
